bc51b047ee3c258bde7ae48e376c9486929f15e3
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-09-17 18:53:54 +0530

    load data using provider

--- lib/widgets/products_grid_view.dart
+++ lib/widgets/products_grid_view.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../models/product.dart';
 import './product_item.dart';
+import '../providers/products.dart';
 
 class ProductsGridView extends StatelessWidget {
-  const ProductsGridView({
-    Key key,
-    @required this.loadedProducts,
-  }) : super(key: key);
-
-  final List<Product> loadedProducts;
-
   @override
   Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+
     return GridView.builder(
       padding: EdgeInsets.all(10.0),
-      itemCount: loadedProducts.length,
+      itemCount: products.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 3 / 2,
         mainAxisSpacing: 10,
         crossAxisSpacing: 10,
       ),
-      itemBuilder: (ctx, i) => ProductItem(loadedProducts[i].id,
-          loadedProducts[i].title, loadedProducts[i].imageUrl),
+      itemBuilder: (ctx, i) => ProductItem(
+        products[i].id,
+        products[i].title,
+        products[i].imageUrl,
+      ),
     );
   }
 }
