05143d1e9ac8b99361aed8b20ec9c1654b4399a5
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-09-17 16:06:55 +0530

    pass data using named routes

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  final String title;
-  const ProductDetailScreen(this.title, {Key key}) : super(key: key);
+  // final String title;
+  const ProductDetailScreen({Key key}) : super(key: key);
+
+  static const routeName = 'detail-screen';
 
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('title'),
       ),
     );
   }
