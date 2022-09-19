3de3ec39a7d2317b597f711fca5889e64755430c
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-09-19 15:59:03 +0530

    listen only to changing widget using provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -5,50 +5,40 @@ import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  // final String id;
-  // final String title;
-  // final String imageUrl;
-
-  // ProductItem(
-  //   this.id,
-  //   this.title,
-  //   this.imageUrl,
-  // );
-
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    return Consumer<Product>(
-      builder: (context, value, child) => ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
-                  arguments: product.id);
-            },
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover,
-            ),
+    final product = Provider.of<Product>(context, listen: false);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
           ),
-          footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: IconButton(
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            builder: (context, value, _) => IconButton(
               icon: Icon(product.isFavourite
                   ? Icons.favorite
                   : Icons.favorite_outline_sharp),
               onPressed: () {
                 product.isFavToggle();
               },
-              color: Theme.of(context).colorScheme.secondary,
+              //color: Theme.of(context).colorScheme.secondary,
             ),
-            title: Text(product.title),
-            trailing: IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: () {},
-              color: Theme.of(context).colorScheme.secondary,
-            ),
+          ),
+          title: Text(product.title),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            // color: Theme.of(context).colorScheme.secondary,
           ),
         ),
       ),
