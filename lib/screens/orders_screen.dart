5a230f127543d6115d180e41417cdfdc0e44b996
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-09-20 22:41:53 +0530

    oders screen added

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
