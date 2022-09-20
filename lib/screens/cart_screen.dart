f469cd27ba22b5f6db08d65dce24a4550a08cab8
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-09-21 00:47:19 +0530

    add orders item widget expanded part

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -33,7 +33,7 @@ class CartScreen extends StatelessWidget {
                   ),
                   Chip(
                     label: Text(
-                      '\$${cart.totalAmount}',
+                      '\$${cart.totalAmount.toStringAsFixed(2)}',
                       style: TextStyle(color: Colors.white),
                     ),
                     backgroundColor: Theme.of(context).colorScheme.primary,
