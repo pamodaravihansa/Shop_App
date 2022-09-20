db7345b864866d07b40cc45b899cace854a5a522
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-09-20 23:18:33 +0530

    add order item widget

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,13 +1,25 @@
-
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/orders.dart' show Orders;
+
+import '../widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
+    final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+      ),
+    );
   }
 }
