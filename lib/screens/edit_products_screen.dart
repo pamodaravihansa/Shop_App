bb1ca4371d133cc5e9239635ef41f58a9b3f04c9
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-09-21 15:28:19 +0530

    added edit products screen

---
+++ lib/screens/edit_products_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  const EditProductScreen({Key key}) : super(key: key);
+
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Products'),
+      ),
+      body: Container(),
+    );
+  }
+}
